37eda8bd812fc1a874c9b1df8b36e6caa1a9e8fe
Amy Silva <amyambard@icloud.com>
2018-10-01 16:02:07 -0700

    Remove logo

--- website/src/App.js
+++ website/src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import MapContainer from './MapContainer';
-import logo from './AmyLogo.png';
 import './App.css';
 
 class App extends Component {
@@ -8,7 +7,6 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Neighborhood Map</h1>
         </header>
 
