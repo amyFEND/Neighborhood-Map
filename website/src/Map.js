52f599c23e867ad74cbdf0c57772e11e4eeaf359
Amy Silva <amyambard@icloud.com>
2018-10-02 11:23:52 -0700

    Remove unnecesary code

--- website/src/Map.js
+++ website/src/Map.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import ReactDOM from 'react-dom';
 import { withScriptjs, withGoogleMap, GoogleMap, Marker } from "react-google-maps"
 
 const MyMapComponent = withScriptjs(withGoogleMap((props) =>
--- website/src/MapContainer.js
+++ website/src/MapContainer.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import ReactDOM from 'react-dom';
 import Map from './Map';
 
 class MapContainer extends Component {
