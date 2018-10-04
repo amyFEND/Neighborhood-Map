7d629c809555d8dd4a6cc8abfac089226c9afe46
Amy Silva <amyambard@icloud.com>
2018-10-04 13:51:01 -0700

    Add hamburger and close menu icons

--- website/src/Navigation.js
+++ website/src/Navigation.js
@@ -4,6 +4,9 @@ class Navigation extends Component {
   render() {
     return (
       <header className="App-header">
+        <button className="hamburger">&#9776;</button>
+        <button className="cross">&#735;</button>
+
         <h1 className="App-title">Neighborhood Map</h1>
       </header>
     )
