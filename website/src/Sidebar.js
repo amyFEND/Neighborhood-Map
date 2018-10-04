a5284177035076f5e99f68028fa89bcb481bfdd3
Amy Silva <amyambard@icloud.com>
2018-10-03 18:11:42 -0700

    Style: add space between code

--- website/src/Sidebar.js
+++ website/src/Sidebar.js
@@ -7,15 +7,20 @@ class Sidebar extends Component {
     return (
       <div id="sidebar">
         <button className="infoBtn">Info</button>
+
         <input type='text' name='search' placeholder='Filter results' className="input" />
+
         <select className="select">
           <option disabled value="select">Filter by park...</option>
           <option value="all">All</option>
           <option value="disneyland">Disneyland Park</option>
           <option value="californiaAdventure">Disney California Adventure</option>
         </select>
+
         <hr />
+
         <div className="location-info">Location information here</div>
+        
       </div>
     )
   }
