c152010f98fa25a320831e8cedfc4de2ac426326
Amy Silva <amyambard@icloud.com>
2018-10-04 17:16:58 -0700

    Toggle between hamburger and close icons

--- website/src/App.js
+++ website/src/App.js
@@ -6,11 +6,26 @@ import './App.css';
 import './Responsive.css';
 
 class App extends Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      isToggleOn: true
+    };
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    this.setState(prevState => ({
+      isToggleOn: !prevState.isToggleOn
+    }));
+  }
+
   render() {
     return (
       <div className="App">
-        <button className="hamburger">&#9776;</button>
-        <button className="cross">&#735;</button>
+        <button className={this.state.isToggleOn ? 'hamburger visible' : 'cross visible'} onClick={this.handleClick}>
+          {this.state.isToggleOn ? String.fromCharCode(0x2630) : String.fromCharCode(0x02DF)}
+        </button>
 
         <Navigation showSidebar={this.showSidebar} />
 
